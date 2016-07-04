dfcd450180632dcd2893463f8e40f33a05a26631
Deka <dekachan16@gmail.com>
2016-07-04 21:57:33 +0900

    po

--- src/mazeVisualizer/maze/Maze.java
+++ src/mazeVisualizer/maze/Maze.java
@@ -8,19 +8,19 @@ import java.util.Scanner;
 import mazeVisualizer.Main;
 
 public class Maze {
-	
-	int w, h, size;
+
+	int w, h, leftUpX, leftUpY, size;
 	char[][] map;
 	Point s, t, p;
-	
+
 	boolean[][] passed;
-	
+
 	public Maze(String path) {
 		Scanner sc = new Scanner(Main.class.getClassLoader().getResourceAsStream("res/maze/" + path));
-		
+
 		h = sc.nextInt();
 		w = sc.nextInt();
-		
+
 		map = new char[h][w];
 		for (int y = 0; y < h; y++) {
 			map[y] = sc.next().toCharArray();
@@ -35,22 +35,23 @@ public class Maze {
 				}
 			}
 		}
-		
+
 		sc.close();
-		
+
 		// チップサイズ
 		size = Math.min(Main.WINDOW_WIDTH / w / 8 * 8, Main.WINDOW_HEIGHT / h / 8 * 8);
-		
+
+		leftUpX = (Main.WINDOW_WIDTH - w * size) / 2;
+		leftUpY = (Main.WINDOW_HEIGHT - h * size) / 2;
+
 		passed = new boolean[h][w];
-		
+
 		p = new Point(s);
 	}
-	
+
 	/** 描画 */
 	public void draw(Graphics2D g) {
-		int leftUpX = (Main.WINDOW_WIDTH - w * size) / 2;
-		int leftUpY = (Main.WINDOW_HEIGHT - h * size) / 2;
-		
+
 		for (int y = 0; y < h; y++) {
 			for (int x = 0; x < w; x++) {
 				switch (map[y][x]) {
@@ -68,14 +69,14 @@ public class Maze {
 						break;
 				}
 				g.fillRect(leftUpX + size * x, leftUpY + size * y, size, size);
-				
+
 				g.setColor(new Color(255, 0, 0, 50));
 				if (passed[y][x]) g.fillRect(leftUpX + size * x, leftUpY + size * y, size, size);
 			}
 		}
-		
+
 		g.setColor(Color.ORANGE);
 		g.fillOval(leftUpX + size * p.x + 2, leftUpY + size * p.y + 2, size - 4, size - 4);
 	}
-	
+
 }
